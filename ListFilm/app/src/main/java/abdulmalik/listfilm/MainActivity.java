7949ed30c1c18291778f463bbf4627a308987a62
abdulmalik1878 <abdulmalik1878@gmail.com>
2017-02-09 10:03:37 +0700

    listfilm

---
+++ ListFilm/app/src/main/java/abdulmalik/listfilm/MainActivity.java
@@ -0,0 +1,57 @@
+package abdulmalik.listfilm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String [] fileTitles= {"The Thor", "Harry Potter", "Inception",
+                                    "wolf of wallstreet", "Django Unchained",
+                                    "Titanic", "Captain America", "Doctor Strange",
+                                    "X-Men Apocalypse", "Shutter Island"};
+
+    private ArrayList<Movie> movies = new ArrayList<>();
+
+    private void iniMovie(){
+        movies.add(new Movie("The Thor", "Film tentang superhero dengan senjata palu", 7.5, 2012));
+        movies.add(new Movie("Hulk", "Superhero yang bisa berubah menjadi raksasa hijau" + "sepert monster", 6.5, 2011));
+        movies.add(new Movie("Spiderman", "Film tentang superhero yang bisa mengeluarkan jaring laba - laba", 7, 2010));
+        movies.add(new Movie("Titanic", "Film yang menceritakan tentang seorang pemuda dan gadis yang saling jatuh cinta" +
+                "di perahu yang bernama Titanic", 8, 2008));
+        movies.add(new Movie("The Martial", "Film yang mengisahkan seorang astronot dari Nasa" +
+                "yang melakukan perjalanan ke planet Mars kemudian peawatnya rusak ", 8.5, 2015));
+        movies.add(new Movie("Harry Potter", "Film tentang pesulap ", 7.5, 2011));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iniMovie();
+        ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this,
+                android.R.layout.simple_list_item_1, movies);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //        android.R.layout.simple_list_item_1, fileTitles);
+        ListView listView = (ListView) findViewById(R.id.list_film);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Movie selectMovie = movies.get(position);
+                Intent intent = new Intent(getBaseContext(), DetailActivity.class);
+                intent.putExtra("abdulmalik.listfilm", selectMovie);
+                startActivity(intent);
+            }
+        });
+    }
+}
